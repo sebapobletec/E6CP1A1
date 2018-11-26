00c8b6922024530e337808926e9f37b3a26336f2
Sebastian Poblete <sebapobletec@gmail.com>
2018-11-26 20:38:12 -0300

    Ejercicio3

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,24 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+
+i=1
+
+while i<991
+  if (990%i == 0)
+    puts i
+  end
+  i = i + 1
+end
+
+for j in 1..990
+  if (990%j == 0)
+    puts j
+  end
+end
+
+
+990.times do |k|
+  if (990%(k+1) == 0)
+    puts k+1
+  end
+end
